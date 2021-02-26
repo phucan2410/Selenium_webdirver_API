5eb59a25ce9346776987d2b8045f4a87625c69aa
An nguyen <nguyenphucan2410@gmail.com>
2021-02-26 17:45:20 +0700

    Update 26/02

---
+++ src/api/Topic_08_Customer_DropDown.java
@@ -0,0 +1,99 @@
+package api;
+
+import org.testng.annotations.Test;
+
+import org.testng.annotations.BeforeClass;
+import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Topic_08_Customer_DropDown {
+	WebDriver driver;
+	WebDriverWait exlicitWait;
+	
+  
+  @BeforeClass
+  public void beforeClass() {
+	  driver = new FirefoxDriver();
+	  
+	  exlicitWait = new WebDriverWait(driver, 30);
+	  
+  }
+  
+  @Test
+  public void TC_01_JQuery() {
+	  driver.get("https://jqueryui.com/resources/demos/selectmenu/default.html");
+	  SelectItemCustomDropDown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "13");
+	  Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(), "13");
+	  sleepInSecound(4);
+	  SelectItemCustomDropDown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "1");
+	  Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(), "1");
+	  
+	  sleepInSecound(4);
+	  SelectItemCustomDropDown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "19");
+	  Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(), "19");
+	  sleepInSecound(4);
+	  SelectItemCustomDropDown("//span[@id='number-button']", "//ul[@id='number-menu']//div", "5");
+	  Assert.assertEquals(driver.findElement(By.xpath("//span[@id='number-button']//span[@class='ui-selectmenu-text']")).getText(), "5");
+	  sleepInSecound(4);
+  }
+  
+  public void TC_03_Angular() {
+	  driver.get("https://ej2.syncfusion.com/angular/demos/#/material/drop-down-list/data-binding");
+	  SelectItemCustomDropDown("//ejs-dropdownlist[@id='games']", allItemXpath, string);
+	  
+  }
+  
+  public void SelectItemCustomDropDown(String parentXpath, String allItemXpath, String string) {
+	  
+	  // chờ cho các item đc hiểm thị ra trước khi chọn
+	  driver.findElement(By.xpath(parentXpath)).click();
+	  sleepInSecound(1);
+	  
+	  // lấy hết tất cả các item đưa vào 1 list
+	  exlicitWait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath(allItemXpath)));
+	  List<WebElement> allItem = driver.findElements(By.xpath(allItemXpath));
+	  
+	  // Dùng vòng lặp duyệt qua từng Item
+	  
+	  for (WebElement item : allItem) {
+		  //Duyệt qua từng item
+		  //text get ra = text mong muốn thì dừng lại 
+		  //thoát khỏi vòng lặp
+		  if(item.getText().equals(string)) {
+			  item.click();
+			  break;
+			  
+		  }
+	  }
+//	  exlicitWait.until()
+  }
+  
+  // Hàm sleep
+  public void sleepInSecound(long timeout) {
+	  try {
+		Thread.sleep(timeout * 1000);
+		
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+		// TODO: handle exception
+	}
+  }
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
